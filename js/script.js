2f8175f7b71725417f7c6430b7bd8291f7c63345
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2022-10-07 12:21:25 -0600

    Removed old question data to display the new one

--- js/script.js
+++ js/script.js
@@ -5,9 +5,11 @@ var welcomeEl = document.querySelector("#welcomeMessageEl");
 var quizQuestionEl = document.querySelector("#quizQuestionEl");
 var quizAnswersEl = document.querySelector("#quizAnswersEl");
 var currentRightAnswer = 0;
+var quizQuestionCurrent = 1;
 var systemMessage = document.querySelector("#system-message");
 var ticker = document.querySelector("#ticker");
 var getStartedBtn = document.querySelector("#getStarted");
+var userScore = 0;
 const quizHighScores = [];
 var quizStarted = false;
 
@@ -27,6 +29,16 @@ function getAnswerOrder() {
 
 // Display the Question and Answers
 function showQandA(qnum) {
+    
+    // If old question is up, remove it
+    if(quizQuestionEl) {
+        systemMessage.innerHTML = "";
+        var answerList = quizAnswersEl.querySelectorAll(':scope > li');
+        console.log(answerList);
+        // Loop through the old list items and remove them
+        answerList.forEach(listItem => listItem.remove());
+    }
+
     var currentQuestion = "quizQuestion" + qnum;
     quizQuestionEl.innerHTML = eval(currentQuestion)[0];
 
@@ -67,30 +79,29 @@ quizAnswersEl.addEventListener("click", function(event) {
     var element = event.target;
 
     if(element.matches("span") && !quizStarted) {
+
         // Set up and start the quiz
 
         welcomeEl.setAttribute("style", "display:none"); // Hide the welcome message
-        element.closest("li").setAttribute("style", "display:none"); // Hide the button
+        element.closest("li").remove(); // Remove the button list item
         quizQuestionEl.setAttribute("style", "display:block"); // Show the Question element
-        showQandA(2); // Display the first question
+        showQandA(quizQuestionCurrent); // Display the first question
         setTimer();   // Start the timer
         quizStarted = true;
+
     } else {
         
-        // var chosenAnswer = document.querySelector('#quizQuestionEl :nth-child(' + Number(currentRightAnswer) + ')');
-        // var chosenAnswer = document.querySelector('#quizAnswersEl div:nth-child(2)');
-        //console.log("system thinks it's " + chosenAnswer);
-        //var chosenAnswer = toString(element).charAt(5);
-        //var chosenAnswer = index(eval(element));
-        var chosenAnswer = event.target.getAttribute('data-qNumber');
-       // event.target.getAttribute('data-bar')
-        console.log(chosenAnswer);
+        var chosenAnswer = event.target.getAttribute('data-qNumber'); // Check with answer the user picked
 
         if(Number(chosenAnswer) === Number(currentRightAnswer)){
             systemMessage.innerHTML = "<hr />Correct!";
+            userScore++;
         } else {
             systemMessage.innerHTML = "<hr />Wrong!";
+            quizTimer -= 10; // Take 10 seconds off the timer
         }
+        quizQuestionCurrent++;
+        setTimeout(() => { showQandA(quizQuestionCurrent); }, "1000"); // Ask the next question
     }
 });
 
