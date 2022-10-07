9c950fa73ba9166b5015db0633988cacd70d7c92
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2022-10-07 03:52:37 -0600

    Randomized quiz answers

--- js/script.js
+++ js/script.js
@@ -3,21 +3,46 @@ var quizTimer = 75;
 var quizScore = 0;
 var quizQuestionEl = document.querySelector("#quizQuestionEl");
 var quizAnswersEl = document.querySelector("#quizAnswersEl");
+var currentRightAnswer = 0;
 var systemMessage = document.querySelector("#system-message");
 const quizHighScores = [];
+
+
+// Get a random order for the answers
+function getAnswerOrder() {
+    var numArray = [];
+    while(numArray.length < 4) {
+        var mynum = (Math.floor(Math.random() * 4)) + 1; 
+        if(!numArray.includes(mynum)) {
+            numArray.push(mynum);
+        }
+    }
+    return(numArray);
+}
 
 
 // Display the Question and Answers
 function showQandA(qnum) {
     var currentQuestion = "quizQuestion" + qnum;
     quizQuestionEl.innerHTML = eval(currentQuestion)[0];
+
+    // Get a random order for the answers
+    var ansOrder = getAnswerOrder();
     
-    for(var i=1; i < 5; i++) {
+    //use ansOrder instead of for loop
+    for (var i = 0; i < 4; i++) {
+        var answerNumber = ansOrder[i];
+        if(answerNumber === 1) {
+            currentRightAnswer = i + 1;
+        }
         var quizAnswer = document.createElement("li");
-        quizAnswer.textContent = eval(currentQuestion)[i];
+        var ansOrderNum = ansOrder[i];
+        quizAnswer.textContent = eval(currentQuestion)[ansOrderNum];
         quizAnswersEl.appendChild(quizAnswer);
     }
+    console.log("The right answer is " + currentRightAnswer);
 }
+
 
 showQandA(2);
 systemMessage.innerHTML = "Correct";
