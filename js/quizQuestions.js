2d3898d1d338f6e337c3eb75c5f9e95a9c767b48
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2022-10-07 02:31:19 -0600

    Made html elements dynamic and set up question structure

---
+++ js/quizQuestions.js
@@ -0,0 +1,23 @@
+var numOfQuestions = 3;
+
+var quizQuestion1 = [
+    "What was wrong with Papa Bear's bed?",
+    "Too hard",
+    "Too Soft",
+    "Nothing at all",
+    "It was wet."
+];
+var quizQuestion2 = [
+    "What was wrong with Mama Bear's bed?",
+    "Too Soft",
+    "Too hard",
+    "Nothing at all",
+    "It was wet."
+];
+var quizQuestion3 = [
+    "What was wrong with Baby Bear's bed?",
+    "Nothing at all",
+    "Too hard",
+    "Too Soft",
+    "It was wet."
+];
--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 console.log('ready.');
+
+
+// Global variables
+var quizTimer = 75;
+var quizScore = 0;
+var quizQuestionEl = document.querySelector("#quizQuestionEl");
+var quizAnswersEl = document.querySelector("#quizAnswersEl");
+var systemMessage = document.querySelector("#system-message");
+const quizHighScores = [];
+
+
+// Display the Question and Answers
+for(var i=0; i < (numOfQuestions + 1); i++) {
+    quizQuestionEl.innerHTML = "How are you?";
+    
+    var quizAnswer1 = document.createElement("li");
+    quizAnswer1.textContent = "I'm fine.";
+    quizAnswersEl.appendChild(quizAnswer1);
+
+    systemMessage.innerHTML = "Correct";
+}
+
+
+
